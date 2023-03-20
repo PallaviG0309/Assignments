9805a152b76c9f3034caee0db57925c18c03dc90
Gargi <Gargi@DESKTOP-IKLBANM>
2023-03-20 16:38:21 +0530

    updated assign

--- src/assign/SwaValues.java
+++ src/assign/SwaValues.java
@@ -5,20 +5,24 @@ public class SwaValues {
 	public static void main(String[] args) 
 	{
 		int i = 10;
-	    int j = 7;
+	//    int j = 7;
+
+	    i = i++ + i + ++i ;
+
+	    System.out.println(i);
+
 	    
 /*     Swap using 3rd variable k
        int k;
-        k=i;
+        k=i; 
         i=j;
         j=k;*/
 	    /* swap without using 3rd variable*/
-	    i = i + j ;// 10 +7 = i value is 17
+	/*    i = i + j ;// 10 +7 = i value is 17
 	    j = i - j ;// (i=17) -(j=10) = 7
 	    i = i -j; // 17 - 7 = 10
 	    System.out.println(i);
-	    System.out.println(j);
-
+	    System.out.println(j);*/
 	    // Expected Result , i = 7  and j = 10    
 	}
 
---
+++ src/assign/UnaryOperator.java
@@ -0,0 +1,27 @@
+package assign;
+
+public class UnaryOperator {
+
+	public static void main(String[] args) 
+	{
+		int i1 = -10;
+	      System.out.println(i1); //this will print -10 bcoz we are given i1 values is -10 
+
+	      int i2 = 10;
+	      System.out.println(i2++);// this will print 10 bcoz i2 value is 10 and will print 10 
+
+	      int i3 = 10;
+	      System.out.println(++i3);// this will print 11 bcoz i3 value is 10 will increase by 1  
+
+	      int i4 = 10;
+	      System.out.println(i4--);//this will print 10 bcoz it will first assign then decrese
+
+	      int i5 = 10;
+	      System.out.println(--i5);//this will print 9 bcoz i5 value is 10 first decrese value and then assign
+
+	  /*    int i6 = 10;
+	      System.out.println(~i6);//this will print -11*/
+
+	}
+
+}
