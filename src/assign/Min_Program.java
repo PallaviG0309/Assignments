e010cadcda2518ecb85ed9f9ca450de61860b3fa
Ram <Ram@RSPersonal>
2023-04-13 23:39:43 +0530

    Java Program convestion to hr,min,sec

---
+++ src/assign/Min_Program.java
@@ -0,0 +1,15 @@
+package assign;
+
+public class Min_Program {
+
+	public static void main(String[] args) {
+		    int seconds = 86399;
+		    int Sec= seconds % 60;
+	        int Hr_m = seconds / 60;
+	        int Min = Hr_m % 60;
+	        int	Hr = Hr_m / 60;
+	        System.out.print( Hr + "Hour  " + Min + "Minute  " + Sec +  "second");
+
+	}
+
+}
