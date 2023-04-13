ac073ce30336c779ee259318fc0d7b827d4d18bb
Ram <Ram@RSPersonal>
2023-04-13 21:54:41 +0530

    Increment Operator or conditional operator

---
+++ src/assign/Increment.java
@@ -0,0 +1,24 @@
+package assign;
+
+public class Increment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+			double db1 = 14.5d;
+			db1++;
+			System.out.println(db1);
+			
+			int i = 10;
+			i++;
+			System.out.println(i);
+									
+			boolean bool = true;
+			int a = (bool) ? 1:0;
+			System.out.println(bool);
+
+
+	}
+
+}
