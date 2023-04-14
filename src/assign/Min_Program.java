1012ba86bda29c9cab3332871c3390fbf4d15439
Ram <Ram@RSPersonal>
2023-04-14 15:05:03 +0530

    Program

--- src/assign/Min_Program.java
+++ src/assign/Min_Program.java
@@ -5,9 +5,9 @@ public class Min_Program {
 	public static void main(String[] args) {
 		    int seconds = 86399;
 		    int Sec= seconds % 60;
-	        int Hr_m = seconds / 60;
-	        int Min = Hr_m % 60;
-	        int	Hr = Hr_m / 60;
+	        int Hr1 = seconds / 60;
+	        int Min = Hr1 % 60;
+	        int	Hr = Hr1 / 60;
 	       System.out.print( Hr + "Hour  " + Min + "Minute  " + Sec +  "second");
 
 	}
